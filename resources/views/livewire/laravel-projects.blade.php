0a87fbffdcc3f81bdaa6e1a5e0c48d9f73fbd538
subhendu <bhattasubhendu@gmail.com>
2023-12-24 15:25:53 +0545

    design improvements

--- resources/views/livewire/laravel-projects.blade.php
+++ resources/views/livewire/laravel-projects.blade.php
@@ -6,28 +6,22 @@
            class="mb-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
            type="button" wire:click="selectProjectAction">Select Project</button>
     </div>
-{{--    <form wire:submit="create">--}}
-{{--        {{ $this->form }}--}}
 
-{{--        <button--}}
-{{--            class=" my-2 bg-red-500 text-white px-3 rounded-full "--}}
-{{--            type="submit">--}}
-{{--            Submit--}}
-{{--        </button>--}}
-{{--    </form>--}}
 
-    <h2 class="text-2xl">All Projects</h2>
+    <h2 class=" border-y-2 py-2 text-2xl font-bold mb-3">All Projects</h2>
     <ul>
         @foreach($projects as $project)
             <li>
-                {{$project->name}} , Path - {{$project->path}}
+                <span class="font-bold text-xl">
+                    {{$project->path}}
+                </span>
 
                 <button
-                    class="mb-3 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-3 rounded-full"
-                    wire:click="visitProject({{$project->id}})">Go to project</button>
+                    class=" ml-5 mb-3 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-3 rounded "
+                    wire:click="visitProject({{$project->id}})">Open project</button>
 
                 <button
-                    class="mb-3 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-3 rounded-full"
+                    class="mb-3 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-3 rounded"
                     wire:confirm="Are you sure you want to delete this?"
                     wire:click="deleteProject({{$project->id}})">Delete</button>
             </li>
